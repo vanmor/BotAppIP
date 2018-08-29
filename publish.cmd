9421e3fc439a946693656b249fd09575bb15d3e0
Ivan Parsikov <vanmor@hotmail.de>
2018-08-29 14:11:04 +0200

    BotAppAnfang_290818

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=botappip-Web-Deploy.pubxml -p:Password=l9uBvs8767YAtXnMefTXl40tuaXTgEP2g6hp85A55D5twmtintGBpwf3Poxb
+
